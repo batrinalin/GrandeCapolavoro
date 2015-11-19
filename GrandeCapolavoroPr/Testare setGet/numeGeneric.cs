860b47425d0b5ff6cfbdd274ea0c1ca57bd5152a
Alin <batrin_alin@yahoo.com>
2015-11-19 09:27:05 -0800

    Commit 1.04
    Am adaugat un proiect cu SetGet

---
+++ GrandeCapolavoroPr/Testare setGet/numeGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testare_setGet
+{
+    class numeGeneric
+    {
+        private string nume;
+
+        public string NumeGeneric
+        {
+            get { return nume; }
+            set { nume = value; }
+        }
+
+        public numeGeneric(string nume)
+        {
+            this.nume = nume;
+        }
+    }
+}
